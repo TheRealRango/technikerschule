d25854b9efd1466921d97f3240899fb3168ab65b
Lorenz <76167654+l0r3n2f@users.noreply.github.com>
2021-01-14 13:17:23 +0100

    Added Wuerfelspiel

---
+++ Softwareentwicklung/Wuerfelspiel/src/de/its/lf/Spieler.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.its.lf;
+
+/**
+ *
+ * @author lorenz
+ */
+public class Spieler {
+//Attribute
+    private String name;
+    private int punkte;
+//constructor
+    public Spieler() {
+    }
+
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPunkte(int punkte) {
+        this.punkte = punkte;
+    }
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    public int getPunkte() {
+        return punkte;
+    }
+
+}
---
+++ Softwareentwicklung/Wuerfelspiel/src/de/its/lf/Wuerfel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.its.lf;
+
+import java.lang.Math;
+
+/**
+ *
+ * @author lorenz
+ */
+public class Wuerfel {
+// Attribute
+
+    private int zahl;
+//Constructor
+
+    public Wuerfel() {
+    }
+//getter 
+
+    public int getZahl() {
+        return zahl;
+    }
+
+    public void werfen() {
+        zahl = (int) (Math.random() * 6+1);
+        
+
+    }
+}
